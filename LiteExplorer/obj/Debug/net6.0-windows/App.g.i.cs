6a11d49978ce26179c283f02276a5f78d99fb477
abaye <8140hp@gmail.com>
2024-05-24 16:45:33 +0300

    v1.3.0b

--- LiteExplorer/obj/Debug/net6.0-windows/App.g.i.cs
+++ LiteExplorer/obj/Debug/net6.0-windows/App.g.i.cs
@@ -59,7 +59,7 @@ namespace LiteExplorer {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/LiteExplorer;V1.0.0.0;component/app.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/LiteExplorer;component/app.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\App.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
